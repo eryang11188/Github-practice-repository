b06a0e389740138002e74fdc48f2f78e58e42cf4
jungiryang <eryang1118@gmail.com>
2025-12-05 01:42:38 +0900

    주석 추가 및 각 printf문에 줄바꿈 추가

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 # include <stdio.h>
 
 int main(void){
-printf("Git practice start");
+printf("Git practice start\n");
 
 // test2 브랜치 추가
-printf("첫번째 커밋용 예제 printf문 출력");
+printf("첫번째 커밋용 예제 printf문 출력\n");
 
+// 두번째 커밋용 - 각 printf문에 줄바꿈 추가
 
 return 0;
 }
